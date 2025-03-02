fc98108f26fa7b7c8bfd0e910c7ee795d703f57c
mia <mia.haworth@informatik.hs-fulda.de>
2025-03-02 12:11:03 +0100

    Calculator App Panel and Button Layout

---
+++ src/Calculator.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Calculator {
+    int boardWidth = 360;
+    int boardHeight = 540;
+
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGrey = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customPink = new Color(255, 186, 236);
+
+    String[] buttonValues = {
+            "AC", "+/-", "%", "÷",
+            "7", "8", "9", "×",
+            "4", "5", "6", "-",
+            "1", "2", "3", "+",
+            "0", ".", "√", "="
+    };
+    String[] rightSymbols = { "÷", "×", "-", "+", "=" };
+    String[] topSymbols = { "AC", "+/-", "%" };
+
+    JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
+
+    // constructor
+    Calculator() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null); // center frame
+        frame.setResizable(false); // user cannot resize window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // terminates program when pressing x Button
+        frame.setLayout(new BorderLayout()); // place components n o s w in frame
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel, BorderLayout.NORTH); // entire displayPanel is placed north on the screen
+
+        buttonsPanel.setLayout(new GridLayout(5, 4));
+        buttonsPanel.setBackground(customBlack);
+        frame.add(buttonsPanel);
+
+        for (int i = 0; i < buttonValues.length; i++) {
+            JButton button = new JButton();
+            String buttonValue = buttonValues[i];
+            button.setFont(new Font("Arial", Font.PLAIN, 30));
+            button.setText(buttonValue);
+            button.setFocusable(false); // get rid of lines around symbols
+            buttonsPanel.add(button);
+        }
+    }
+}
