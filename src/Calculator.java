bb3a2b4d01d42a2d3b5dd3c24c0ad7708f12fb0c
mia <mia.haworth@informatik.hs-fulda.de>
2025-03-04 23:11:01 +0100

    customized buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
+//Code by Kenny Yip Coding
 public class Calculator {
     int boardWidth = 360;
     int boardHeight = 540;
@@ -58,8 +59,43 @@ public class Calculator {
             button.setFont(new Font("Arial", Font.PLAIN, 30));
             button.setText(buttonValue);
             button.setFocusable(false); // get rid of lines around symbols
-
+            button.setBorder(new LineBorder(customBlack));
+            // customize buttons
+            if (Arrays.asList(topSymbols).contains(buttonValue)) { // if currentsymbol is part of topsymbols
+                button.setBackground(customLightGray);
+                button.setForeground(customBlack); // fontcolor
+            } else if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+                button.setBackground(customPink);
+                button.setForeground(Color.BLACK);
+            } else {
+                button.setBackground(customDarkGrey);
+                button.setForeground(Color.WHITE);
+            }
             buttonsPanel.add(button);
+
+            button.addActionListener(new ActionListener() {
+                // when mouseclick, action is performed and e refers to the action
+                public void actionPerformed(ActionEvent e) {
+                    // where does action come from, source of the click is a jbutton
+                    JButton button = (JButton) e.getSource();
+                    // get symbol of math operation
+                    String buttonValue = button.getText();
+
+                    if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+
+                    } else if (Arrays.asList(topSymbols).contains(buttonValue)) {
+
+                    } else { // digits or decimal
+                        if (buttonValue == ".") {
+
+                        } else if ("123456789".contains(buttonValue)) {
+                            if (displayLabel.getText() == "0") {
+
+                            }
+                        }
+                    }
+                }
+            });
         }
     }
 }
